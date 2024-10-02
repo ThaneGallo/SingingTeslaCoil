cea54ca77e4f9fc3e2b97af67ce252ee9f8e2ccf
Thane <thanegallo@gmail.com>
2024-10-02 13:02:20 -0400

    added struct

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define HEADER_SIZE
 
 typedef struct MIDI_header_chunk
 {
@@ -28,27 +27,36 @@ uint8_t parse_midi(FILE *fp)
 
     // all big endian
     hdr.MThd = (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
-    if(hdr.MThd != 0x4D546864){ // string of MThd
-        printf("Unexpected Header - Recieved: %c", hdr.MThd); //return characters for easy read
+    if (hdr.MThd != 0x4D546864)
+    {                                                         // string of MThd
+        printf("Unexpected Header - Recieved: %c", hdr.MThd); // return characters for easy read
         return -1;
     }
+    
 
     hdr.hdr_len = (buf[4] << 24) | (buf[5] << 16) | (buf[6] << 8) | buf[7];
-    if(hdr.hdr_len != 0x0006){ // always 6 bytes
+    if (hdr.hdr_len != 0x0006)
+    { // always 6 bytes
         printf("Unexpected Header Length - Recieved: %u", hdr.hdr_len);
         return -1;
     }
 
     hdr.format = (buf[8] << 8) | buf[9];
-    if(hdr.format != (0x00 | 0x01 | 0x02)){
+    if (hdr.format != 0x00 && hdr.format != 0x01 && hdr.format != 0x02)
+    {
         printf("Unexpected Header Format - Recieved: %u", hdr.format);
         return -1;
     }
 
-
     hdr.num_tracks = (buf[10] << 8) | buf[11];
     hdr.division = (buf[12] << 8) | buf[13];
 
+    // to check all header info
+    printf("Header string is: %x\n", hdr.MThd);
+    printf("Length is: %x\n", hdr.hdr_len);
+    printf("Format is:%x \n", hdr.format);
+    printf("Number of tracks is: %x\n", hdr.num_tracks);
+    printf("Division is: %x\n", hdr.division);
 
     return 0;
 }
@@ -57,14 +65,19 @@ int main(int argc, char *argv[])
 {
     FILE *fp; // for midi file operations
 
+    
+
     fp = fopen(argv[1], "r");
 
     if (!fp)
     {
-        return NULL;
+        printf("fp is Null");
+        return -1;
     }
 
     parse_midi(fp); // grabs header info
+
+
 
     return 0;
 }
